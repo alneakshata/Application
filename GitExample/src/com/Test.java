40f2254827b56264f0dbac5be14ee9f8944c661a
alneakshata <alneakshata209@gmail.com>
2023-01-27 00:07:05 +0530

    GitExample

---
+++ GitExample/src/com/Test.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First Git Example by using Eclipse");
+		
+		Student student=new Student();
+		student.setId(10);
+		student.setName("Akshata");
+		student.setAddress("Latur");
+		
+		System.out.println("Student details: "+student);
+	}
+}
