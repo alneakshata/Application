dbfe728a04ba484f778a49767daff8f13f784e03
alneakshata <123493991+alneakshata@users.noreply.github.com>
2023-01-27 23:45:23 +0530

    Create Employee.java

---
+++ GitExample/src/com/Employee.java
@@ -0,0 +1,20 @@
+public class Employee{
+
+private int id;
+private String name;
+  
+public void setId(int id1){
+  id=id1;
+}
+public int getId(){
+return id;
+}
+
+public void setName(String name){
+  this.name=name;
+}
+public String getName(){
+return name;
+}
+  
+}
