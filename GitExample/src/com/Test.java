37182fcbf1728b0ddb4144f0d43f5c03c10bc4a8
alneakshata <alneakshata209@gmail.com>
2023-02-05 23:31:19 +0530

    Employee data set in Test

--- GitExample/src/com/Test.java
+++ GitExample/src/com/Test.java
@@ -3,14 +3,19 @@ package com;
 public class Test {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("First Git Example by using Eclipse");
-		
-		Student student=new Student();
+
+		Student student = new Student();
 		student.setId(10);
 		student.setName("Akshata");
 		student.setAddress("Latur");
-		
-		System.out.println("Student details: "+student);
+
+		System.out.println("Student details: " + student);
+
+		Employee employee = new Employee();
+		employee.setId(101);
+		employee.setName("Akash");
+		System.out.println("Employee details: " + employee);
 	}
 }
